0a2caa0ca4f527defa757492fde7fabe6d7310c3
Sophia <sophiansl2018@yahoo.com>
2022-11-05 01:43:30 +1100

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Heroimg from "./components/Heroimg";
-
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div className="App">
      <Navbar />
      <Heroimg />
+     <Carousel />
+    
     </div>
   );
 }
